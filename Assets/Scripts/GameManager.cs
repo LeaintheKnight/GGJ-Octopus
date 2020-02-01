0e9b19d5b47bc3c9d5b6446e1f27664d2493481e
LeaintheKnight <Gnaster30@gmail.com>
2020-02-01 12:56:09 -0800

    otherScripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int numberOfTurtlesSaved;
+    int trashCollected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void collectTrash(){
+
+    }
+    void gameOver(){
+
+    }
+
+    void numberOfTurtlesLeft(){
+
+    }
+}
