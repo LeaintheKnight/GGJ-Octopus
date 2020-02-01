9cb622552084d692faa2ef2017578cd1ab56b5a1
JGerdes <gerdesjaren@gmail.com>
2020-02-01 12:50:27 -0800

    Most animations

--- Assets/Scripts/CharacterMvmt.cs
+++ Assets/Scripts/CharacterMvmt.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CharacterMvmt : MonoBehaviour
 {
 
-    public float speed = 3.5f;
+    public float speed = 30f;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -18,16 +18,16 @@ public class CharacterMvmt : MonoBehaviour
     {
         if (Input.GetKey("a"))
         {
-            rb.AddForce(new Vector3(-1, 0, 0) * speed);
+            rb.AddForce(Vector3.left * speed);
         }
         else if (Input.GetKey("d"))
         {
-            rb.AddForce(new Vector3(1, 0, 0) * speed);
+            rb.AddForce(Vector3.right * speed);
         }
 
         if (Input.GetKeyDown("space"))
         {
-            rb.AddForce(new Vector3(0, 1, 0) * speed, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
         }
     }
 }
