0aa2cd15f4f96f121aa29883a581411c75275ea3
Victor Zhou <44218140+Fireryrex@users.noreply.github.com>
2020-02-01 14:37:28 -0800

    screw you conan

--- Assets/CreatureScripts/swimMovement.cs
+++ Assets/CreatureScripts/swimMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class swimMovement : MonoBehaviour
+public class SwimMovement : MonoBehaviour
 {
     [SerializeField] Transform moveTo;
     // Start is called before the first frame update
