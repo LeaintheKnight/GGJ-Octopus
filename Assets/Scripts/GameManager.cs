f8c9ab4f1c42a71fd5ea9b77214a33805ac31be6
something999 <wongny1@uci.edu>
2020-02-01 13:01:05 -0800

    Updated Game Manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,4 +27,29 @@ public class GameManager : MonoBehaviour
     void numberOfTurtlesLeft(){
 
     }
+
+    //
+    // The following functions are required for the UI script to display
+    // the amount of trash and number of turtles correctly.
+    //
+
+    public int getTrash() // Returns the amount of trash collected
+    {
+        return 0;
+    }
+
+    public int getTrashMax() // Returns the max amount of trash in the game
+    {
+        return 0;
+    }
+
+    public int getTurtles() // Returns the number of turtles collected
+    {
+        return 0;
+    }
+
+    public int getTurtlesMax() // Returns the max number of turtles in the game
+    {
+        return 0;
+    }
 }
