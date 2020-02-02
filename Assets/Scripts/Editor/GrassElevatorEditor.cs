69d530ec1c83527ffd21d80cfd5274a4a7c94701
Conan Truong <truong.conan@gmail.com>
2020-02-02 14:46:45 -0800

    Grass elevator

---
+++ Assets/Scripts/Editor/GrassElevatorEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(GrassElevator))]
+public class GrassElevatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Elevate Grass"))
+        {
+            string nameSearch = serializedObject.FindProperty("nameSearch").stringValue;
+            float start = serializedObject.FindProperty("raiseSearchPoint").floatValue;
+            float offset = serializedObject.FindProperty("offset").floatValue;
+
+            foreach (GameObject o in FindObjectsOfType<GameObject>())
+            {
+                if (o.name.Contains(nameSearch))
+                {
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(o.transform.position + Vector3.up * start, Vector3.down, out hit, Mathf.Infinity, 1 << 8, QueryTriggerInteraction.UseGlobal))
+                    {
+                        Undo.RecordObject(o, "Moved Grass");
+                        Vector3 pos = hit.point + Vector3.up * offset;
+                        Debug.Log("Moved " + o.name + " to " + pos);
+                        o.transform.position = pos;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GrassElevator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassElevator : MonoBehaviour
+{
+    [SerializeField] string nameSearch;
+    [SerializeField] float raiseSearchPoint;
+    [SerializeField] float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
