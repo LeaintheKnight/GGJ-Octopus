1a0d7c9adde4b2d5ca87eb113f07f799328fb47d
Conan Truong <truong.conan@gmail.com>
2020-02-01 17:35:57 -0800

    Bubble flip thing

---
+++ Assets/Scripts/BubbleFlipThing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleFlipThing : MonoBehaviour
+{
+    SpriteRenderer whale;
+    SpriteRenderer thisOne;
+    float x;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whale = transform.parent.GetComponent<SpriteRenderer>();
+        thisOne = GetComponent<SpriteRenderer>();
+        x = transform.localPosition.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 localPosition = transform.localPosition;
+        localPosition.x = whale.flipX ? x : -x;
+        thisOne.flipX = whale.flipX;
+        transform.localPosition = localPosition;
+    }
+}
