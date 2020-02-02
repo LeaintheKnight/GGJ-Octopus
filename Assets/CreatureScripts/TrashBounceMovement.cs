45537e97bcfe495c709bb3cded966886a8173326
Conan Truong <truong.conan@gmail.com>
2020-02-02 13:02:57 -0800

    Changed trash movement to rigidbody

---
+++ Assets/CreatureScripts/TrashBounceMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBounceMovement : MonoBehaviour
+{
+    [SerializeField] float initialSpeed = 1.25f;
+    [SerializeField] float deceleration = 0.01f;
+    [SerializeField] float descentSpeed = 0.15f;
+
+    float yVel = 0;
+    float initY;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        enabled = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (transform.position.y < initY)
+        {
+            yVel = initialSpeed * Random.Range(0.9f, 1.1f);
+        }
+
+        rb.position += Vector3.up * yVel * Time.fixedDeltaTime;
+        yVel = Mathf.Max(yVel - deceleration, -descentSpeed);
+    }
+
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+}
---
+++ Assets/CreatureScripts/TrashWavyMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashWavyMovement : MonoBehaviour
+{
+    [SerializeField] float amplitude = 0.15f;
+    [SerializeField] float frequency = 0.3f;
+
+    float time = 0;
+    float initY = 0;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        enabled = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initY = transform.position.y;
+        time = Random.Range(0, 360f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 position = rb.position;
+        position.y = initY + Mathf.Sin(time * Mathf.Deg2Rad) * amplitude;
+        rb.position = position;
+        time += Time.fixedDeltaTime * frequency * 360;
+    }
+
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+}
