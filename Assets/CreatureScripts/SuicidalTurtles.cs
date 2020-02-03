036584b82bd5673c92a2f02859c0d3e8f98b49d8
Conan Truong <truong.conan@gmail.com>
2020-02-02 16:43:15 -0800

    They kill themeselves

---
+++ Assets/CreatureScripts/SuicidalTurtles.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicidalTurtles : MonoBehaviour
+{
+    [SerializeField] float waitTimeMin = 5;
+    [SerializeField] float waitTimeMax = 10;
+    [SerializeField] float minSpeed = 1;
+    [SerializeField] float maxSpeed = 5;
+
+    Rigidbody rb;
+    [SerializeField] float wait;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        wait = Random.Range(waitTimeMin, waitTimeMax);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        wait -= Time.fixedDeltaTime;
+
+        if (wait <= 0)
+        {
+            rb.velocity = Random.onUnitSphere * Random.Range(minSpeed, maxSpeed);
+            wait += Random.Range(waitTimeMin, waitTimeMax);
+        }
+    }
+}
